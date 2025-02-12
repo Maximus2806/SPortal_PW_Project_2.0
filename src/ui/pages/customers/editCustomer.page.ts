29fb13706c72dec841b03c24141356802e2c2507
Maximus2806 <marozaumaksim@gmail.com>
2025-02-13 00:00:30 +0100

    UI update smoke

--- src/ui/pages/customers/editCustomer.page.ts
+++ src/ui/pages/customers/editCustomer.page.ts
@@ -3,8 +3,13 @@ import { AddNewCustomerPage } from './addNewCustomer.page';
 export class EditCustomerPage extends AddNewCustomerPage {
   uniqueElement = '#delete-customer-btn';
   readonly 'Delete customer button' = this.findElement('#delete-customer-btn');
+  readonly 'Save changes button' = this.findElement('#save-customer-changes');
 
   async clickOnDeleteCustomerButton() {
     await this.click(this['Delete customer button']);
+  }
+
+  async clickOnSaveChangesButton() {
+    await this.click(this['Save changes button']);
   }
 }
