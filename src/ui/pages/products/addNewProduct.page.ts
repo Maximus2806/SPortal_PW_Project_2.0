a16f2b6843a45e4149f7d97c337bcf93c2b027da
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2025-02-16 23:48:54 +0200

    ..

--- src/ui/pages/products/addNewProduct.page.ts
+++ src/ui/pages/products/addNewProduct.page.ts
@@ -5,5 +5,4 @@ export class AddNewProductPage extends AddEditProductPage {
   readonly uniqueElement = '//h2[.="Add New Product "]';
   readonly 'Save Product button' = this.findElement('#save-new-product');
   readonly 'Page body' = this.findElement('//div[@id="root"]/div');
-
 }
