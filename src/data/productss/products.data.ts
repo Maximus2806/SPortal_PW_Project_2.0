163034e0d52d79259ff01c0fead9691b60249f68
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2025-02-20 00:14:33 +0200

    ..

--- src/data/productss/products.data.ts
+++ src/data/productss/products.data.ts
@@ -5,4 +5,3 @@ export const productToastMessages = {
   'assigned to order': `Not allowed to delete product, assigned to the order`,
   'already exist': (name?: string) => `Product with name '${name}' already exists`
 };
-//
