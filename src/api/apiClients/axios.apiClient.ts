241fb89694e3b8f891e9ccad9ad63d9625ca42b4
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2025-02-20 21:24:01 +0200

    reportApiRequest as async

--- src/api/apiClients/axios.apiClient.ts
+++ src/api/apiClients/axios.apiClient.ts
@@ -17,7 +17,7 @@ export class AxiosApiClient {
         return this.transformResponse();
       }
     } finally {
-      // reportApiRequest<T>(options, this.transformResponse());
+      reportApiRequest<T>(options, this.transformResponse());
     }
   }
 
--- src/utils/reporter/api.ts
+++ src/utils/reporter/api.ts
@@ -1,33 +1,47 @@
 import * as allure from 'allure-js-commons';
 import { IRequestOptions, IResponse, IResponseFields } from '../../data/types/api.types';
 
-export function reportApiRequest<T extends IResponseFields>(requestOptions: IRequestOptions, response: IResponse<T>) {
-  logApiRequest(requestOptions);
-  logApiResponse(requestOptions, response);
-}
+export const logApiRequest = async (requestOptions: IRequestOptions) => {
+  await allure.step(
+    `Request: ${requestOptions?.method?.toUpperCase()} ${requestOptions?.url}`,
+    async () => {
+      await allure.attachment(
+        'Request Headers',
+        JSON.stringify(requestOptions?.headers, null, 2),
+        'application/json',
+      );
+      await allure.attachment(
+        'Request Body',
+        requestOptions?.data
+          ? JSON.stringify(requestOptions?.data, null, 2)
+          : '{}',
+        'application/json',
+      );
+    },
+  );
+};
 
-export function logApiRequest(requestOptions: IRequestOptions) {
-  allure.step(`Request: ${requestOptions?.method?.toUpperCase()} ${requestOptions?.url}`, () => {
-    allure.attachment('Request Headers', JSON.stringify(requestOptions?.headers, null, 2), 'application/json');
-    allure.attachment(
-      'Request Body',
-      requestOptions?.data ? JSON.stringify(requestOptions?.data, null, 2) : '{}',
-      'application/json'
-    );
-  });
-}
+export const logApiResponse =
+async <T extends IResponseFields>(requestOptions: IRequestOptions, response: IResponse<T>)  => {
+  await allure.step(
+    `Response: ${response?.status} status, ${requestOptions?.url}`,
+    async () => {
+      await allure.attachment(
+        'Response Headers',
+        JSON.stringify(response?.headers, null, 2),
+        'application/json',
+      );
+      await allure.attachment(
+        'Response Body',
+        JSON.stringify(response?.body, null, 2),
+        'application/json',
+      );
+    },
+  );
+};
 
-export function logApiResponse<T extends IResponseFields>(requestOptions: IRequestOptions, response: IResponse<T>) {
-  allure.step(`Response: ${response?.status} ${requestOptions?.url}`, () => {
-    allure.attachment('Response Headers', JSON.stringify(response?.headers, null, 2), 'application/json');
-    allure.attachment('Response Body', JSON.stringify(response?.body, null, 2), 'application/json');
-
-    // Устанавливаем статус шага
-    if (response && response.status >= 500) {
-      throw new Error(`API Response failed with status: ${response.status}`);
-      //   allure.setStatus("failed");
-      // } else {
-      //   allure.setStatus("passed");
-    }
-  });
-}
+export const reportApiRequest = async <T extends IResponseFields>
+(requestOptions: IRequestOptions, response: IResponse<T>) => {
+  await logApiRequest(requestOptions);
+  await logApiResponse(requestOptions, response);
+};
