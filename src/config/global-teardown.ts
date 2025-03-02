7889e41da12afdfc03530620e811d1632037ad5c
Maximus2806 <marozaumaksim@gmail.com>
2025-03-02 13:23:24 +0100

    Telegram notifications

--- src/config/global-teardown.ts
+++ src/config/global-teardown.ts
@@ -2,13 +2,12 @@ import notificationService from '../services/notification.service';
 import { CI } from './environment';
 import { test as teardown } from '../../src/fixtures/apiServices.fixture';
 
+const message = 'Test run funished: link to report: https://maximus2806.github.io/SPortal_PW_Project_2.0/report/';
 teardown('SLACK NOTIFICATION', async () => {
   console.log('TEARDOWN NOTIFICATION - LAUNCHED');
   if (CI) {
-    await notificationService.postNotification(
-      `Test run funished:
-       link to report: https://maximus2806.github.io/SPortal_PW_Project_2.0/report/`
-    );
+    await notificationService.postNotification(message);
+    await notificationService.sendTelegramNotification(message);
   }
   console.log('TEARDOWN NOTIFICATION - FINISHED');
 });
--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,11 +1,17 @@
 import slackService from './slack.service';
+import { sendNotification as sendTelegramMessage } from './telegram.service';
 
 class NotificationService {
-  constructor(private service = slackService) {}
+  constructor(private service = slackService, private telegram = sendTelegramMessage) {}
 
   async postNotification(notification: string) {
     await this.service.postNotification(notification);
   }
+
+  async sendTelegramNotification(notification: string) {
+    await this.telegram(notification);
+  }
+
 }
 
 export default new NotificationService();
